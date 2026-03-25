41bb1d3dc06d31950595fcc1a4aba9ee3a45636b
shirleyd22 <sd.parralesp@uea.edu.ec>
2026-03-25 11:07:18 -0500

    Limpieza completa con gitignore

--- lista_tareas_app/modelos/tarea.py
+++
@@ -1,7 +0,0 @@
-class Tarea:
-    def __init__(self, id, descripcion):
-        self.id = id
-        self.descripcion = descripcion
-        self.completado = False
-    def marcar_completado(self):
-        self.completado = True
